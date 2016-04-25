88db9f0c4bb5c8586ec51c435a52c1bb1219152a
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2016-04-25 20:34:16 +0300

    Create The Descent.py

---
+++ The-Descent/The Descent.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while True:
+    for i in xrange(8):
+        mountain_h = int(raw_input())  # represents the height of one mountain, from 9 to 0.
+
+    # Write an action using print
+    # To debug: print >> sys.stderr, "Debug messages..."
+
+    # The number of the mountain to fire on.
+    print "4"
