27eed7a55b5f427b59e943a2539a5034daa74a5d
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2016-04-25 20:39:31 +0300

    Update The Descent.py

--- The-Descent/The Descent.py
+++ The-Descent/The Descent.py
@@ -7,11 +7,16 @@ import math
 
 # game loop
 while True:
+    index = 0
+    mt_max_height = -1 
     for i in xrange(8):
         mountain_h = int(raw_input())  # represents the height of one mountain, from 9 to 0.
-
+        if mountain_h > mt_max_height:
+            # i find the highest mountain and by the end of the turn i shoot it
+            mt_max_height = mountain_h
+            index = i
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
-
+    
     # The number of the mountain to fire on.
-    print "4"
+    print index
